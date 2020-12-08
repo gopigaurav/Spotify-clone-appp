d3ab6db395d6aaa36dfc1b9321d22967176ebb02
unknown <gopigaurav9@gmail.com>
2020-12-08 11:30:38 +0530

    Updates

---
+++ precache-manifest.bb648b4a80462c70781d20d080b01afe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc65e8a4c148d6193be7aca62b264ac",
+    "url": "/Spotify-clone-app/index.html"
+  },
+  {
+    "revision": "2b4cf59ea091d85a881e",
+    "url": "/Spotify-clone-app/static/css/main.2625a82f.chunk.css"
+  },
+  {
+    "revision": "b03b4e8dcb51ac19580f",
+    "url": "/Spotify-clone-app/static/js/2.5ae8ea6e.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/Spotify-clone-app/static/js/2.5ae8ea6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4cf59ea091d85a881e",
+    "url": "/Spotify-clone-app/static/js/main.9c60ab1a.chunk.js"
+  },
+  {
+    "revision": "72933be01202ae7f7606",
+    "url": "/Spotify-clone-app/static/js/runtime-main.f4bed082.js"
+  }
+]);
